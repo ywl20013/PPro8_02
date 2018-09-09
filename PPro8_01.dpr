06860c829215b3f39bc0a26b1bc34493e06688ca
ywl10013@126.com <ywl10013@126.com>
2018-09-10 06:27:17 +0800

    second commit

--- PPro8_01.dpr
+++ PPro8_01.dpr
@@ -2,10 +2,11 @@ program PPro8_01;
 
 uses
   Vcl.Forms,
-  UI.Main in 'UI.Main.pas' {fmMain} ,
+  UI.Main in 'UI.Main.pas' {fmMain},
   Models.L2 in 'Models\Models.L2.pas',
-  UI.Test in 'UI.Test.pas' {fmTest} ,
-  DateTimeHelper in 'DateTimeHelper.pas';
+  UI.Test in 'UI.Test.pas' {fmTest},
+  DateTimeHelper in 'DateTimeHelper.pas',
+  Data.Provider.UDP in 'Data.Provider.UDP.pas' {dmProviderUDP: TDataModule};
 
 {$R *.res}
 
@@ -15,6 +16,7 @@ begin
   Application.MainFormOnTaskbar := True;
   // Application.CreateForm(TfmMain, fmMain);
   Application.CreateForm(TfmTest, fmTest);
+  Application.CreateForm(TdmProviderUDP, dmProviderUDP);
   Application.Run;
 
 end.
