0337c6407e39426625a886025ac59d603cd7f451
ywl10013@126.com <ywl10013@126.com>
2018-09-09 22:27:14 +0800

    second commit

---
+++ UI.Test.pas
@@ -0,0 +1,24 @@
+unit UI.Test;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TfmTest = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fmTest: TfmTest;
+
+implementation
+
+{$R *.dfm}
+
+end.
