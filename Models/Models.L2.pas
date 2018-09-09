77175426715b6502093059a3364fd186aa7dd493
ywl10013@126.com <ywl10013@126.com>
2018-09-09 22:25:46 +0800

    second commit

--- Models/Models.L2.pas
+++ Models/Models.L2.pas
@@ -3,6 +3,36 @@ unit Models.L2;
 interface
 
 type
+  {
+    Register?symbol=ZVZZT.NQ&feedtype=L2 & output=[bykey|bytype
+
+    API将接收代码ZVZZT.NQ的Level 2的所有报价更新。这些更新将被写入PPro8登录目录的文档，文档名为：L2_1_ZVZZT.NQ.log
+    L2的数据必须包含代码这个参数。
+    L2的数据，每个代码分别写入一个文档。
+    每个Level 2的更新，是一个按逗号分隔的数据行，栏目包括：
+    LocalTime=08:39:43.114 ← 代表数据到达本地电脑的时间，按本地电脑的时间显示
+    MarketTime=08:39:42.601 ← 市场更新时间
+    Mmid=ANON ← 代表本次更新的市场参与者代码MMID
+    Side=B ← 代表更新的订单的方向
+    Price=8.6 ← 代表更新的订单的价格
+    Volume=100 ← 代表更新的订单的股数
+    Depth=1 ← 代表订单的价位数
+    SequenceNumber=27003 ← 这个序列号，是按每个市场参与者代码MMID、Price订单价格、和方向Side分配的，可以用来排除错乱的数据。
+
+    每一次的更新，要么 (a) 创建一个新的价位，要么 (b) 对指定的市场参与者代码MMID, 方向Side和价格Price组合作出更新。
+
+    举例：如果您当前有一个在ANON市场参与者代码中的400股，定价在每股8.6美元的买单，上面的例子将把ANON的8.6价位的买单股数调整到100股。
+    Market Depth Snapshot市场深度截图
+    当API在一个代码上注册Level 2的数据源的时候，它将首先获取一个截图。截图代表了这个代码目前的报价状态，并且将随着实时的更新消息不断更新。
+    截图的数据从Side=s开始。举例：
+    LocalTime=08:37:31.908,MarketTime=00:00:00.000,Mmid=,Side=s,Price=0,Volume=0,Depth=0,SequenceNumber=0
+    所有截图的数据都显示SequenceNumber=0.
+    截图的数据从Side=e结束，举例：
+    LocalTime=08:37:31.908,MarketTime=00:00:00.000,Mmid=,Side=e,Price=0,Volume=0,Depth=0,SequenceNumber=0
+  }
+  /// <summary>
+  /// Market Depth (L2)市场深度
+  /// </summary>
   TL2Line = class
   private
     FMarketTime: TDateTime;
--- PPro8_01.dpr
+++ PPro8_01.dpr
@@ -3,7 +3,8 @@ program PPro8_01;
 uses
   Vcl.Forms,
   UI.Main in 'UI.Main.pas' {fmMain},
-  Models.L2 in 'Models\Models.L2.pas';
+  Models.L2 in 'Models\Models.L2.pas',
+  UI.Test in 'UI.Test.pas' {fmTest};
 
 {$R *.res}
 
@@ -11,5 +12,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfmMain, fmMain);
+  Application.CreateForm(TfmTest, fmTest);
   Application.Run;
 end.
