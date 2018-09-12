369cb7885d153ee8974e51477d6ae76ee6484172
ywl10013@126.com <ywl10013@126.com>
2018-09-12 15:18:19 +0800

    first commit

---
+++ PPro8_02.dpr
@@ -0,0 +1,29 @@
+program PPro8_02;
+
+uses
+  Vcl.Forms,
+  UI.Main in 'UI.Main.pas' {fmMain} ,
+  Models.L2 in 'Models\Models.L2.pas',
+  UI.Test in 'UI.Test.pas' {fmTest} ,
+  Helper.TDateTime in 'Helper\Helper.TDateTime.pas',
+  Net.UDP.Test in 'Net\UDP\Net.UDP.Test.pas' {dmProviderUDPTest: TDataModule} ,
+  Net.UDP.Server in 'Net\UDP\Net.UDP.Server.pas' {dmProviderUDPServer: TDataModule} ,
+  Data.Provider.Sqlite.L2 in 'Data\Provider\Sqlite\Data.Provider.Sqlite.L2.pas' {dmDataProviderSqliteL2: TDataModule} ,
+  Data.Provider in 'Data\Provider\Data.Provider.pas' {DataProvider: TDataModule} ,
+  Data.Provider.Sqlite in 'Data\Provider\Sqlite\Data.Provider.Sqlite.pas' {dmDataProviderSqlite: TDataModule};
+
+{$R *.res}
+
+
+begin
+  ReportMemoryLeaksOnShutdown := True;
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  // {$IFDEF DEBUG}
+  // Application.CreateForm( TfmTest, fmTest );
+  // {$ELSE}
+  Application.CreateForm( TfmMain, fmMain );
+  // {$ENDIF}
+  Application.Run;
+
+end.
